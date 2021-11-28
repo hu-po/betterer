e9fd34c3ba025c68d032d5ad08aafe77ae37e100
Hugo <8493427+hu-po@users.noreply.github.com>
2021-11-27 20:07:11 -0800

    random word combos

---
+++ leetcode/random_word.py
@@ -0,0 +1,17 @@
+import requests
+import random
+
+num_combos = 10
+num_words = 5
+min_word_len = 4
+max_word_len = 10
+
+word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+response = requests.get(word_site)
+words = response.content.splitlines()
+
+words = [_ for _ in words if len (_) >= min_word_len]
+words = [_ for _ in words if len (_) <= max_word_len]
+
+for _ in range(num_combos):
+  print('.'.join([random.choice(words) for _ in range(num_words)]))
