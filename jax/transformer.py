373e3a3d62e65e671ebf12dc6e66a06e302bfd6f
HugoCMU <hugo.p.cmu@gmail.com>
2022-01-21 10:46:37 -0800

    scaled dot product attention.

--- jax/transformer.py
+++ jax/transformer.py
@@ -8,9 +8,11 @@ https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/transformer.py
 
 """
 
+from typing import Callable
 import numpy as np
 import tracemalloc
 import time
+import math
 import jax
 from jax import numpy as jnp
 import torch
@@ -19,63 +21,84 @@ import torch
 def main():
     # input sequence (batch, seq, feature)
     batch_size = 1
-    seq_size = 2
+    dk = 2 # dimmensionality k
     feat_size = 4
-    mu, sigma = 0, 0.1
+    mu, sigma = 0, 1
 
-    # query vectors
-    dim_query = 12
+    # Query (q) and Key (k) vectors have dimmensionality k
+    q = np.random.normal(loc=mu, scale=sigma, size=(batch_size, dk, feat_size))
+    k = np.random.normal(loc=mu, scale=sigma, size=(batch_size, dk, feat_size))
+    
+    # Values vector has dimmentionality 
+    v = None
 
-    # Use the same input sequence for both functions
-    seq = np.random.normal(loc=mu, scale=sigma, size=(
-        batch_size, seq_size, feat_size))
-
-    jax_transformer(name='JAX')
-    torch_transformer(name='PyTorch')
+    jax_self_attention(q, k, v, dk)
+    torch_self_attention(q, k, v, dk)
 
 
-def print_evaluation(func):
+def print_evaluation(_func : Callable) -> Callable:
     """ Provide memory and runtime prints for a transformer function. """
     def wrapped(*args, **kwargs):
-        name = kwargs.get('name', 'DEFAULT')
         tracemalloc.start()
         start_time = time.time()
-        wrapped(*args, **kwargs)
-        print(f'{name} transformer runtime was {time.time() - start_time} seconds.')
-        print(f'{name} transformer used {tracemalloc.get_traced_memory()}')
+        _func(*args, **kwargs)
+        print(f'{_func.__name__} transformer runtime was {time.time() - start_time} seconds.')
+        print(f'{_func.__name__} transformer used {tracemalloc.get_traced_memory()}')
         tracemalloc.stop()
     return wrapped
 
 
 @print_evaluation
-def jax_transformer(
-    seq: np.ndarray,
-    num_heads: int = 2,
-    num_encoder_layers: int = 2,
+def jax_self_attention(
+    q: np.ndarray,
+    k: np.ndarray,
+    v: np.ndarray,
+    dk: int,
+    **kwargs,
 ) -> np.ndarray:
     """
-        Transformer block implemented in JAX.
+        Self-Attention block implemented in JAX.
     """
     # convert input numpy sequence to tensor
-    seq: jnp.DeviceArray = jnp.array(seq)
-    out: jnp.DeviceArray = jax.nn.gelu(seq)
+    q: jnp.DeviceArray = jnp.array(q)
+    k: jnp.DeviceArray = jnp.array(k)
+    out: jnp.DeviceArray = jnp.dot(q, jnp.transpose(k, axes=(0, 2, 1)))
+    # normalize by square root of dimmensionality
+    out = out / math.sqrt(dk)
+    # softmax
+
+    # multiply by values
+
+
+    # out: jnp.DeviceArray = jax.nn.gelu(seq)
     # convert back to numpy array
     return np.array(out)
 
 
 @print_evaluation
-def torch_transformer(
-    seq: np.ndarray,
-    num_heads: int = 2,
-    num_encoder_layers: int = 2,
+def torch_self_attention(
+    q: np.ndarray,
+    k: np.ndarray,
+    v: np.ndarray,
+    dk: int,
+    **kwargs,
 ) -> np.ndarray:
     """
-        Transformer block implemented in PyTorch.
+        Self-Attention block implemented in PyTorch.
     """
     # convert input numpy sequence to tensor
-    seq: torch.Tensor = torch.from_numpy(seq)
-    activation = torch.nn.GELU()
-    out: torch.Tensor = activation(seq)
+    q: torch.Tensor = torch.from_numpy(q)
+    k: torch.Tensor = torch.from_numpy(k)
+    out: torch.Tensor = torch.dot(q, torch.transpose(k, 2, 1))
+    # normalize by square root of dimmensionality
+    out = out / math.sqrt(dk)
+    # softmax
+
+    # multiply by values
+
+
+    # activation = torch.nn.GELU()
+    # out: torch.Tensor = activation(seq)
     # transformer_model = torch.nn.Transformer(
     #     nhead=num_heads, num_encoder_layers=num_encoder_layers)
     # out = transformer_model(seq, tgt)
