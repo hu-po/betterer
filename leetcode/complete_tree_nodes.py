17c0444490299e1ea3992275cc76b90997dcacba
Hugo <8493427+hu-po@users.noreply.github.com>
2022-01-31 16:21:50 -0600

    Create complete_tree_nodes.py

---
+++ leetcode/complete_tree_nodes.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        
+        # time O(N)
+        # space O(D) where D is depth of tree logN
+        
+        if not root:
+            return 0
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)    
+    
+        
+        
