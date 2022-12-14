a99d47c499daf64e736f26466c9ade659725a7f8
HugoCMU <hugo.p.cmu@gmail.com>
2022-12-14 08:53:37 -0600

    batch size fix

--- kaggle/enzymes/run_embeddor.sh
+++ kaggle/enzymes/run_embeddor.sh
@@ -1,21 +1,21 @@
-python embeddor.py --csv_input_file="test.csv" \
---model="esm2_t33_650M_UR50D" \
---csv_output_file="test_embeddings_esm2_t33_650M_UR50D.csv" \
---batch_size=1
+# python embeddor.py --csv_input_file="test.csv" \
+# --model="esm2_t33_650M_UR50D" \
+# --csv_output_file="test_embeddings_esm2_t33_650M_UR50D.csv" \
+# --batch_size=1
 
-python embeddor.py --csv_input_file="train_filtered.csv" \
---model="esm2_t33_650M_UR50D" \
---csv_output_file="train_filtered_embeddings_esm2_t33_650M_UR50D.csv" \
---batch_size=1
+# python embeddor.py --csv_input_file="train_filtered.csv" \
+# --model="esm2_t33_650M_UR50D" \
+# --csv_output_file="train_filtered_embeddings_esm2_t33_650M_UR50D.csv" \
+# --batch_size=1
 
 python embeddor.py --csv_input_file="test.csv" \
 --model="esm1v_t33_650M_UR90S_1" \
---csv_output_file="test_embeddings_esm1v_t33_650M_UR90S_1.csv"
+--csv_output_file="test_embeddings_esm1v_t33_650M_UR90S_1.csv" \
 --batch_size=1
 
 python embeddor.py --csv_input_file="train_filtered.csv" \
 --model="esm1v_t33_650M_UR90S_1" \
---csv_output_file="train_filtered_embeddings_esm1v_t33_650M_UR90S_1.csv"
+--csv_output_file="train_filtered_embeddings_esm1v_t33_650M_UR90S_1.csv" \
 --batch_size=1
 
 # python embeddor.py --csv_input_file="train_filtered.csv" \
